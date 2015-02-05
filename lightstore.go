2e0c29204c521a720a99f6ca6dc81f50eee37909
Sergey Romanov <xxsmotur@gmail.com>
2015-02-06 02:28:13 +0500

    Change module to skiplist

--- lightstore.go
+++ lightstore.go
@@ -1,38 +1,37 @@
-package main
+package lightstore
 
 import 
 (
 	"fmt"
 	"sync"
-	"github.com/gansidui/skiplist"
+	"github.com/ryszard/goskiplist/skiplist"
 	"time"
 	//"runtime"
 	//"errors"
 )
+
+
+//Basic implmentation of key-value store
+
 const 
 (
 	param = 0
 )
 
 type Settings struct {
-	innerdata string
+	Innerdata string
 }
 
 type Dict struct {
-	value map[string] interface{}
+	Value map[string] interface{}
 }
 
 func Newdict()(*Dict){
+	d := new(Dict)
+	d.Value = make(map[string] interface{})
 	return new (Dict)
 }
 
-func Insert(key string, data interface{}){
-
-}
-
-func Get(key string){
-	
-}
 
 type Store struct{
 	items int
@@ -46,26 +45,35 @@ type User struct {
     id    string
 }
 
-
-func (st*Store) Get(value string) {
+type Value struct {
+	id float64
+	key string
+	value interface {}
+}
+func (st*Store) Get(value string)interface{} {
 	st.lock.Lock()
 	defer st.lock.Unlock()
 
 	switch st.mainstore.(type){
 	case Dict:
 		fmt.Println("not implement yet")
+	case BMtree:
+		fmt.Println("This is bmtree")
 	default:
-		fmt.Println(st.mainstore.(*skiplist.SkipList).GetElementByRank(1).Value)
+		result, ok := st.mainstore.(*skiplist.SkipList).Get(value)
+		if ok {
+			return result
+		}
 	}
+	return nil
 }
 
-func (st*Store) Set(key string){
+func (st*Store) Set(key string, value interface{}){
 	switch st.mainstore.(type){
 	case Dict:
 		fmt.Println("A")
 	default:
-		ust := &User{0.6, key}
-		st.mainstore.(*skiplist.SkipList).Insert(ust)
+		st.mainstore.(*skiplist.SkipList).Set(key, value)
 	}
 }
 
@@ -73,8 +81,8 @@ func (st*Store) Remove(key string){
 	switch st.mainstore.(type){
 	case Dict:
 		fmt.Println("not implemented yet")
-	default:
-		st.mainstore.(*skiplist.SkipList).Delete(key)
+	/*default:
+		st.mainstore.(*skiplist.SkipList).Delete(key)*/
 	}
 }
 
@@ -90,61 +98,17 @@ func InitLightStore(settings Settings)(*Store){
 	store := new(Store)
 	fmt.Println("Start working: ", time.Now())
 	store.items = 0;
-	if settings.innerdata =="skiplist"{
-		store.mainstore = skiplist.New()
+	if settings.Innerdata =="skiplist"{
+		store.mainstore = skiplist.NewStringMap()
 	}
 
-	if settings.innerdata =="dict"{
+	if settings.Innerdata =="dict"{
 		store.mainstore = Newdict()
 	}
 
-	if settings.innerdata == "b-tree" {
-		
+	if settings.Innerdata == "b-tree" {
+		store.mainstore = InitBMTree()
 	}
 	store.lock = mutex
 	return store
-}
-
-
-func (u *User) Less(other interface{}) bool {
-    if u.score > other.(*User).score {
-        return true
-    }
-    if u.score == other.(*User).score && len(u.id) > len(other.(*User).id) {
-        return true
-    }
-    return false
-}
-
-func test_sync(){
-	mutex := &sync.Mutex{}
-	go func(){
-		total := 0
-		for i := 0; i < 100; i++ {
-			mutex.Lock()
-			total += 1
-			mutex.Unlock()
-		}
-	}()
-}
-
-func main() {
-	/*fmt.Println(runtime.NumCPU())
-	fmt.Println(runtime.NumGoroutine())*/
-	/*us := make([]*User, 7)
-    us[0] = &User{6.6, "hi"}
-    us[1] = &User{3.1, "hi"}
-    us[2] = &User{4.5, "hi"}
-    us[3] = &User{7.3, "hi"}
-	sl := skiplist.New()
-	sl.Insert(us[0])
-	sl.Insert(us[1])
-	sl.Insert(us[2])
-	sl.Insert(us[3])*/
-
-	st:= InitLightStore(Settings{innerdata: "skiplist"})
-	st.Set("first")
-	st.Set("New value")
-	st.Get("first")
-	st.CloseLightStore()
 }
