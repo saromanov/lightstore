d2804e9fe7cebcff0298aeaba7a6036b2be8b311
Sergey Romanov <xxsmotur@gmail.com>
2015-07-11 23:44:15 +0500

    update

--- snapshot.go
+++ snapshot.go
@@ -4,9 +4,11 @@ import
 	"encoding/json"
 	"io/ioutil"
 	"path"
+	"fmt"
+	"strings"
 )
 
-//Basic snapshot for the data
+//Basic snapshot for all data in ligtstore
 
 type snapshot interface {
 	Write(object SnapshotObject)
@@ -30,14 +32,37 @@ func (so* SnapshotObject) Write(object SnapshotObject){
 		panic(err)
 	}
 
-	errwrite := ioutil.WriteFile(path.Join(so.Dir, "snap1"), b, 0777)
+	errwrite := ioutil.WriteFile(path.Join(so.Dir, "snap1.lssnapshot"), b, 0777)
 	if errwrite != nil {
 		panic(err)
 	}
 
 }
 
-func Read(snapshotname string) {
 
+//Read provides reading snapshot and store data to lightstore
+//if snapshotname is ""(empty), load more recently snapshot
+func (so *SnapshotObject) Read(snapshotname string) {
+	data, err := ioutil.ReadFile(path.Join(so.Dir, snapshotname))
+	if err != nil {
+		panic(fmt.Sprintf("Can't find snapshot with the name %s", snapshotname))
+	}
+
+	snapshots := checkAvailableSnapshots(so.Dir)
 }
 
+//Return snapshotnames
+func checkAvailableSnapshots(dir string)[]string{
+	files, err := ioutil.ReadDir(dir)
+	result := []string{}
+	if err != nil {
+		panic(err)
+	}
+
+	for _, pathobj := range files {
+		if strings.HasSuffix(pathobj.Name(), "lssnapshot") {
+			result = append(result, path.Join(dir, pathobj.Name()))
+		}
+	}
+	return result
+}
