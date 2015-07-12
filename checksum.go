8906a03e08c0f415c8e7272ba4bc932ff0ce0956
Sergey Romanov <xxsmotur@gmail.com>
2015-07-12 23:59:36 +0500

    Add hash with gohasha

--- checksum.go
+++ checksum.go
@@ -4,10 +4,19 @@ import
 (
 	"crypto/sha256"
 	"encoding/hex"
+	"github.com/saromanov/gohasha"
 )
 
 func Checksum(data string) string {
 	check := sha256.New()
 	check.Write([]byte(data))
 	return hex.EncodeToString(check.Sum(nil))
+}
+
+func Hash(data string) string {
+	hashstr, err := gohasha.GoHasha(&gohasha.GohashaOptions{Data: data})
+	if err != nil {
+		panic(err)
+	}
+	return hashstr
 }
