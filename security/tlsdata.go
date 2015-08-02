dbf111eb66676bb443ab45a47bf43a766b6d125a
Sergey Romanov <xxsmotur@gmail.com>
2015-08-02 23:50:06 +0500

    Puppet for tls related methods

---
+++ security/tlsdata.go
@@ -0,0 +1,44 @@
+package security
+
+import
+(
+	"io/ioutil"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"crypto/rand"
+	"crypto/rsa"
+)
+
+type TLSData struct {
+
+}
+
+func CreateCertificate(Country, Organization, OrganizationalUnit string){
+	ca := &x509.Certificate{
+		SerialNumber: big.NewInt(1653),
+		Subject: pkix.Name{
+			Country: []string{Country},
+			Organization: []string{Organization},
+			OrganizationalUnit: []string{OrganizationalUnit},
+		},
+		NotBefore: time.Now(),
+		NotAfter: time.Now().AddDate(10,0,0),
+		SubjectKeyId: []byte{1,2,3,4,5},
+		BasicConstraintsValid: true,
+		IsCA: true,
+		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage: x509.KeyUsageDigitalSignature|x509.KeyUsageCertSign,
+	}
+
+	priv, _ := rsa.GenerateKey(rand.Reader, 1024)
+	pub := &priv.PublicKey
+	ca_b, err := x509.CreateCertificate(rand.Reader, ca, ca, pub, priv)
+	if err != nil {
+		log.Println("create ca failed", err)
+		return
+	}
+	ca_f := "ca.pem"
+	log.Println("write to", ca_f)
+	ioutil.WriteFile(ca_f, ca_b, 0777)
+
+}
