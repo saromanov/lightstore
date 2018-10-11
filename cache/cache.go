24e4ef66965c8a9f8a1a1e73d8b4c8e98ae5ea82
Sergey <xxsmotur@gmail.com>
2018-10-11 12:59:05 +0500

    Fixing of import

--- cache/cache.go
+++ cache/cache.go
@@ -1,20 +1,17 @@
 package cache
 
-import
-(
-	"github.com/hashicorp/golang-lru"
-)
+import "github.com/hashicorp/golang-lru"
 
 type Cache struct {
 	lrudata *lru.Cache
 }
 
-func New(cachesize int)*Cache {
+func New(cachesize int) *Cache {
 	cachedata := new(Cache)
 	cachedata.lrudata, _ = lru.New(cachesize)
 	return cachedata
 }
 
-func (cachedata*Cache) AddToCache(item string,timedata int){
+func (cachedata *Cache) AddToCache(item string, timedata int) {
 	cachedata.lrudata.Add(item, timedata)
 }
