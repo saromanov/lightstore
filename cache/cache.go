ef5b7fc262b865b05ea7a875b739fd110fba4974
Sergey <xxsmotur@gmail.com>
2018-10-11 13:00:01 +0500

    using of cache size attribute

--- cache/cache.go
+++ cache/cache.go
@@ -3,11 +3,13 @@ package cache
 import "github.com/hashicorp/golang-lru"
 
 type Cache struct {
-	lrudata *lru.Cache
+	lrudata   *lru.Cache
+	cachesize int
 }
 
 func New(cachesize int) *Cache {
 	cachedata := new(Cache)
+	cachedata.cachesize = cachesize
 	cachedata.lrudata, _ = lru.New(cachesize)
 	return cachedata
 }
