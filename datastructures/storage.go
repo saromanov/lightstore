fc35917bd376d1ac4a0d4e9ba8c23fcb9d64212f
Sergey <xxsmotur@gmail.com>
2018-10-13 15:44:37 +0500

    Add interface for storage

---
+++ datastructures/storage.go
@@ -0,0 +1,9 @@
+package datastructures
+
+// Storage provides basic abstraction over
+// store of key values
+type Storage interface {
+	Get([]byte) ([]byte, error)
+	Put([]byte, []byte) error
+	Delete([]byte) error
+}
