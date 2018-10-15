1ab1c2541de50aa49518086ccd65ae8c1501d398
Sergey <xxsmotur@gmail.com>
2018-10-15 23:50:12 +0500

    Add more tests

--- store/store_test.go
+++ store/store_test.go
@@ -1,10 +1,23 @@
 package store
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestOpen(t *testing.T) {
 	s := Open(nil)
 	if !s.IsCreated() {
 		t.Fatalf("unable to create db")
+	}
+	stored := s.Set([]byte("key"), []byte("value"))
+	if !stored {
+		t.Fatalf("unable to store data")
+	}
+
+	key := []byte("key")
+	data := s.Get(key)
+	value := data.([]byte)
+	if string(key) != string(value) {
+		t.Fatalf("unable to get data")
 	}
 }
