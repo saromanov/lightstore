64d8be8c1fabb44fdb553fcd02211f972b4e00c2
Sergey <xxsmotur@gmail.com>
2018-10-15 23:19:42 +0500

    implementation of btree

--- datastructures/btree.go
+++ datastructures/btree.go
@@ -1,12 +1,16 @@
 package datastructures
 
-import "github.com/google/btree"
+import (
+	"errors"
+
+	"github.com/google/btree"
+)
 
 // BTree provides implementation of btree
 // over google btree package
 type BTree struct {
 	repair *Repair
-	tree   *btree.BTree
+	Value  *btree.BTree
 }
 
 func InitBTree(degree int) *BTree {
@@ -15,4 +19,43 @@ func InitBTree(degree int) *BTree {
 		tree:   btree.New(degree),
 	}
 	return bt
+}
+
+func (d *BTree) Put(key []byte, value interface{}, op ItemOptions) error {
+	if op.Immutable {
+		return nil
+	}
+	d.Value.Set(key, value)
+	return nil
+}
+
+// Get provides getting of value by the key
+func (d *BTree) Get(key []byte) (interface{}, error) {
+	value, ok := d.Value.Get(key)
+	if !ok {
+		return nil, errors.New("unable to find element")
+	}
+	return value, nil
+}
+
+func (d *BTree) GetFromRepair(key string) (*RepairItem, error) {
+	return d.repair.GetFromRepair(key, "")
+}
+
+// Exist provides implementation for checking of key is exist
+func (d *BTree) Exist(key []byte) bool {
+	_, ok := d.Value.Get(key)
+	if !ok {
+		return false
+	}
+	return true
+}
+
+// Remove provides removing of the record
+func (d *SkipList) Delete(key []byte) error {
+	_, ok := d.Value.Delete(key)
+	if !ok {
+		return errors.New("unable to delete element")
+	}
+	return nil
 }
