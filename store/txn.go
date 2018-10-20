d916228270b970cb3cff4ebebf5f31d19e39f24b
Sergey <xxsmotur@gmail.com>
2018-10-20 15:51:22 +0500

    Definition for txn entry

--- store/txn.go
+++ store/txn.go
@@ -1,5 +1,7 @@
 package store
 
+//https://docs.oracle.com/cd/E17275_01/html/api_reference/C/txn.html
+
 // write defines writing to store
 type write struct {
 	key []byte
@@ -10,4 +12,17 @@ type Txn struct {
 	writes []write
 	count  int64
 	id     int64
+	reads  []int64
+}
+
+// Entry defines new key value pair
+type Entry struct {
+	key   []byte
+	value []byte
+}
+
+// NewTransaction creates a new transaction
+func (s *Store) NewTransaction() *Txn {
+	txn := &Txn{}
+	return txn
 }
