3b8420f4a6f8274de031202ed97489e3b5824bea
Sergey <xxsmotur@gmail.com>
2018-11-20 22:44:40 +0500

    Add decompress method

--- store/compression.go
+++ store/compression.go
@@ -1,15 +1,29 @@
 package store
 
 import (
-	"compress/zlib"
 	"bytes"
+	"compress/zlib"
+	"io"
 )
+
 // Compress provides compression of data
-func Compress(data []byte)[]byte {
+func Compress(data []byte) []byte {
 	var b bytes.Buffer
 
 	w := zlib.NewWriter(&b)
 	w.Write(data)
 	w.Close()
 	return b.Bytes()
+}
+
+// Decompress provides decompression of data
+func Decompress(data []byte) []byte {
+	var buf bytes.Buffer
+	b := bytes.NewReader(data)
+	r, err := zlib.NewReader(b)
+	if err != nil {
+		panic(err)
+	}
+	io.Copy(buf, r)
+	return buf.Bytes()
 }
