1379b1cc60ec7f114498ff0238435c0881c682f9
Sergey <xxsmotur@gmail.com>
2018-11-23 23:50:41 +0500

    Add Test for write data

--- store/lightstore_test.go
+++ store/lightstore_test.go
@@ -11,3 +11,18 @@ func TestOpenLightstore(t *testing.T) {
 		t.Fatalf("unable to create db")
 	}
 }
+
+func TestSetData(t *testing.T) {
+	light := Open(nil)
+	defer light.Close()
+	err := light.Write(func(txn *Txn) error {
+		err := txn.Set([]byte("foo"), []byte("bar"))
+		if err != nil {
+			return err
+		}
+		return txn.Commit()
+	})
+	if err != nil {
+		t.Fatalf("unable to write data: %v", err)
+	}
+}
