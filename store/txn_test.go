100836d75d557bab4db6d2b53d6984969a1f80e6
Sergey <xxsmotur@gmail.com>
2018-11-25 17:36:42 +0500

    Add simple test on transaction

--- store/txn_test.go
+++ store/txn_test.go
@@ -1,10 +1,23 @@
 package store
 
 import (
+	"bytes"
 	"testing"
 )
 
 func TestTxnStart(t *testing.T) {
-	txn := NewTransaction(true)
-	txn.Set([]byte("foo"), []byte("bar"))
+	st := newStore(nil)
+	txn := st.NewTransaction(true)
+	err := txn.Set([]byte("foo"), []byte("bar"))
+	if err != nil {
+		t.Fatal("unable to insert data")
+	}
+	err = txn.Commit()
+	if err != nil {
+		t.Fatal("unable to apply commit")
+	}
+	result := txn.Get([]byte("foo"))
+	if bytes.Compare(result, []byte("bar")) != 0 {
+		t.Fatal("unable to get result")
+	}
 }
