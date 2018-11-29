d9a946981909bf4f25a3536d4f2c40a9d20e609b
Sergey <xxsmotur@gmail.com>
2018-11-29 16:15:27 +0500

    Add more metrics

--- monitoring/prometheus.go
+++ monitoring/prometheus.go
@@ -4,19 +4,16 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+const (
+	normMean   = 0.0001
+	normDomain = 0.002
+)
+
 var (
-	rpcDurations = prometheus.NewSummaryVec(
-		prometheus.SummaryOpts{
-			Name:       "rpc_durations_seconds",
-			Help:       "RPC latency distributions.",
-			Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
-		},
-		[]string{"lightstore"},
-	)
 	rpcDurationsHistogram = prometheus.NewHistogram(prometheus.HistogramOpts{
 		Name:    "rpc_durations_histogram_seconds",
 		Help:    "RPC latency distributions.",
-		Buckets: prometheus.LinearBuckets(*normMean-5**normDomain, .5**normDomain, 20),
+		Buckets: prometheus.LinearBuckets(normMean-5*normDomain, .5*normDomain, 20),
 	})
 	opsInsert = prometheus.NewCounter(prometheus.CounterOpts{
 		Name: "lightstore_insert_total",
@@ -26,11 +23,17 @@ var (
 		Name: "lightstore_get_total",
 		Help: "Number of read operations",
 	})
+	gaugeInsert = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name:      "ligthstore_insert_gauge",
+			Help:      "Number of inserts",
+			Subsystem: "storage",
+		},
+	)
 )
 
 // Init provides initialization of the Prometheus
 func Init() {
-	prometheus.MustRegister(rpcDurations)
 	prometheus.MustRegister(rpcDurationsHistogram)
 	prometheus.MustRegister(opsInsert)
 	prometheus.MustRegister(opsGet)
