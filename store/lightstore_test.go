8157bb34e1eb29057312ef8047faf8981823bcd4
Sergey <xxsmotur@gmail.com>
2018-12-02 18:35:53 +0500

    Add test for delete data

--- store/lightstore_test.go
+++ store/lightstore_test.go
@@ -139,3 +139,31 @@ func TestViewData(t *testing.T) {
 		t.Fatalf("unable to get: %v", err)
 	}
 }
+
+func TestDeleteData(t *testing.T) {
+	light := Open(nil)
+	defer light.Close()
+
+	err := light.Write(func(txn *Txn) error {
+		err := txn.Set([]byte("foo"), []byte("bar"))
+		if err != nil {
+			return err
+		}
+		return txn.Commit()
+	})
+	if err != nil {
+		t.Fatalf("unable to write data: %v", err)
+	}
+
+	err = light.Write(func(txn *Txn) error {
+		err := txn.Delete([]byte("foo"))
+		if err != nil {
+			return err
+		}
+		return txn.Commit()
+	})
+	if err != nil {
+		t.Fatalf("unable to write data: %v", err)
+	}
+
+}
