33ba503a151e8e4230b5d8f303a35326fadd77d0
Sergey <xxsmotur@gmail.com>
2018-12-04 23:49:30 +0500

    implementation of the put command

--- cmd/cmd/put.go
+++ cmd/cmd/put.go
@@ -1,19 +1,38 @@
 package cmd
 
 import (
-	"github.com/spf13/cobra"
-	"github.com/saromanov/lightstore/store"
+	"log"
 
+	"github.com/saromanov/lightstore/store"
+	"github.com/spf13/cobra"
 )
+
 var putCmd = &cobra.Command{
 	Use:   "put",
 	Short: "Putting of the key value pair",
-	Long: `Putting of the key value pair on format 'key value'`,
-	Run: func(cmd *cobra.Command, args []string) {
-		ls = store.Open(nil)
-	},
+	Long:  `Putting of the key value pair on format 'key value'`,
+	Run:   put,
 }
 
 func put(cmd *cobra.Command, args []string) {
 	ls := store.Open(nil)
+	if len(args) != 2 {
+		log.Fatal("put command should contains 2 arguments")
+	}
+	key := args[0]
+	value := args[1]
+	err := ls.Write(func(txn *store.Txn) error {
+		err := txn.Set([]byte(key), []byte(value))
+		if err != nil {
+			return err
+		}
+		return txn.Commit()
+	})
+	if err != nil {
+		log.Fatalf("unable to write data: %v", err)
+	}
+}
+
+func init() {
+	rootCmd.AddCommand(putCmd)
 }
