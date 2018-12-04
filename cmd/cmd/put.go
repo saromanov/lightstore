5af49c95deb6fe72adceb09c2f799bbb817a3b7c
Sergey <xxsmotur@gmail.com>
2018-12-04 23:35:46 +0500

    Updating

--- cmd/cmd/put.go
+++ cmd/cmd/put.go
@@ -1,1 +1,19 @@
 package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/saromanov/lightstore/store"
+
+)
+var putCmd = &cobra.Command{
+	Use:   "put",
+	Short: "Putting of the key value pair",
+	Long: `Putting of the key value pair on format 'key value'`,
+	Run: func(cmd *cobra.Command, args []string) {
+		ls = store.Open(nil)
+	},
+}
+
+func put(cmd *cobra.Command, args []string) {
+	ls := store.Open(nil)
+}
