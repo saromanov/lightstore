dd222eb3dd3a175fd6ccad870d2fb83e98c299da
Sergey <xxsmotur@gmail.com>
2019-01-07 17:52:48 +0500

    Checksum: add more tests

--- backend/checksum_test.go
+++ backend/checksum_test.go
@@ -11,5 +11,12 @@ func TestHash(t *testing.T) {
 
 func TestHashEmpty(t *testing.T) {
 	assert.Equal(t, Hash(""),"", "hashes is not equal")
-	recover()
+}
+
+func TestChecksum(t *testing.T) {
+	assert.Equal(t, Checksum("123"),"a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3", "hashes is not equal")
+}
+
+func TestChecksumEmpty(t *testing.T) {
+	assert.Equal(t, Checksum(""),"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855", "hashes is not equal")
 }
