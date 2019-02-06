9b37cf140b7dcaf2a64dc980f82b632b7b3a9852
Sergey <xxsmotur@gmail.com>
2019-02-06 17:11:03 +0500

    implementation of AddSetCommand on writer

--- store/writer.go
+++ store/writer.go
@@ -12,7 +12,20 @@ type Writer struct {
 
 // AddSetCommand defines append data to log file with SET command
 func (w *Writer) AddSetCommand(key, value []byte) error {
-	_, err := w.file.Write(key)
+	if err := w.write([]byte("set")); err != nil {
+		return err
+	}
+	if err := w.write(key); err != nil {
+		return err
+	}
+	if err := w.write(value); err != nil {
+		return err
+	}
+	return w.write([]byte(";"))
+}
+
+func (w *Writer) write(data []byte) error {
+	_, err := w.file.Write(data)
 	if err != nil {
 		return fmt.Errorf("unable to write to file: %v", err)
 	}
