8e329915768b140a6452c1cc4581a993f04885e8
Sergey <xxsmotur@gmail.com>
2019-02-09 19:34:16 +0500

    Complete loading of data

--- store/store.go
+++ store/store.go
@@ -102,7 +102,7 @@ func loadData(st *Store, path string) error {
 		} else if bytes.Compare(line, []byte("end;")) == 0 {
 			if commandSet == true {
 				commandSet = false
-				err := st.Set(key, value)
+				err := st.store.Put(key, value, ds.ItemOptions{})
 				if err != nil {
 					return fmt.Errorf("unable to set data: %v", err)
 				}
